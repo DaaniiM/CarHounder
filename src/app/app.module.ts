f4eafec923e8b9549fcf59b5a306569e035970aa
DaaniiM <danimellado1993@gmail.com>
2021-03-13 17:10:34 +0100

    formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComoFuncionaComponent } from './pages/como-funciona/como-funciona.component';
 import { FooterComponent } from './pages/footer/footer.component';
+import { FormularioContactoComponent } from './pages/formulario-contacto/formulario-contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComoFuncionaComponent,
-    FooterComponent
+    FooterComponent,
+    FormularioContactoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/formulario-contacto/formulario-contacto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-contacto',
+  templateUrl: './formulario-contacto.component.html',
+  styleUrls: ['./formulario-contacto.component.css']
+})
+export class FormularioContactoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
