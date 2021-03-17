d346113b7d8211c4f2f2dd16a3e7e6e27c261e32
DaaniiM <danimellado1993@gmail.com>
2021-03-17 19:05:30 +0100

    modal citas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { ComoFuncionaComponent } from './pages/como-funciona/como-funciona.compo
 import { FormularioContactoComponent } from './pages/formulario-contacto/formulario-contacto.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { PaginaTallerComponent } from './pages/pagina-taller/pagina-taller.component';
-import { PerfilesComponent } from './pages/perfiles/perfiles.component';
 import { ServiciosComponent } from './pages/servicios/servicios.component';
 import { SobreNosotrosComponent } from './pages/sobre-nosotros/sobre-nosotros.component';
 
@@ -14,7 +13,6 @@ const routes: Routes = [
   {path:"comoFunciona", component: ComoFuncionaComponent},
   {path:"formularioContacto", component: FormularioContactoComponent},
   {path:"paginaTaller", component: PaginaTallerComponent},
-  {path:"perfiles", component: PerfilesComponent},
   {path:"servicios", component: ServiciosComponent},
   {path:"sobreNosotros", component: SobreNosotrosComponent},
   {path:"chat", component: ChatComponent}
