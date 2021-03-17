46433c2cbacbfc9b21891d938c7f4a97910cb99e
PedroCorcho <corchogamez.pedro@gmail.com>
2021-03-17 18:43:15 +0100

    Resultado Busqueda

---
+++ src/app/pages/resultado-busqueda/resultado-busqueda.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resultado-busqueda',
+  templateUrl: './resultado-busqueda.component.html',
+  styleUrls: ['./resultado-busqueda.component.css']
+})
+export class ResultadoBusquedaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
