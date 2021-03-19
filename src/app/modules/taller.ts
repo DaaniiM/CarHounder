d684966addf3b84f41b97582f86a21583cbb9146
PedroCorcho <corchogamez.pedro@gmail.com>
2021-03-19 12:37:27 +0100

    add clases usuario y taller

---
+++ src/app/modules/taller.ts
@@ -0,0 +1,7 @@
+export class Taller {
+
+    constructor( public id:number, public email:string, public password:string, public nombre:string, public cif:string, public direccion:string, public cp:number, public ciudad:string, public provincia:string, public telefono:number, public foto:string ) {
+
+    }
+
+}
